c26e185fc8f02dd51ca40c8526ced78f0a87de9c
chalidbaumann <chalid.baumann@stud.hslu.ch>
2022-05-17 09:06:00 +0200

    test sentiment

--- test_sentiment analysis/assets/script/myscript.js
+++ test_sentiment analysis/assets/script/myscript.js
@@ -1,21 +1,33 @@
-function request_sentiment(){
-
-let url = 'https://api.textgain.com/sentiment'
-let base_url = 'https://api.textgain.com/sentimenttag'
-let query = '/test_sentiment analysis/csvjson.json'
-
-
-
-fetch(base_url + query) 
-.then(function (response){ 
-    return response.json()
-})
-.then(function (data){ 
-   build_facts(data)
-
-})
-.catch(function (error){
-    console.log(error)
-})
-}
-
+const language = {
+    de: "de",
+    fr: "fr",
+    it: "it",
+    en: "en",
+  };
+  
+  let query = "some text";
+  
+  const key = "627cb640944da56a6800e80a"; // keep it secret
+  
+  async function get_data() {
+    const response = await fetch(
+      "https://api.textgain.com/sentiment?q=" +
+        query +
+        "&lang=" +
+        language.de +
+        "&key=" +
+        key,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+        mode: "cors",
+      }
+    );
+  
+    const data = await response.json();
+    console.log(data);
+  }
+  
+  get_data();
