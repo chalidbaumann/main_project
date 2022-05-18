bd08f7217eb7f742bb27e32bb41ce68352347c6f
chalidbaumann <chalid.baumann@stud.hslu.ch>
2022-05-18 15:55:41 +0200

    typo test div
    Co-authored-by: sonjaramseier <sonjaramseier@users.noreply.github.com>

--- tests/test_sentiment_analysis/assets/script/myscript.js
+++ tests/test_sentiment_analysis/assets/script/myscript.js
@@ -3,34 +3,33 @@ const language = {
     fr: "fr",
     it: "it",
     en: "en",
-  };
-  
-  let query = "😄 typisch Katze. Ich gehe heute nicht zur Party🙈. Viel Spass!😁 Oooh dir auch.";
+};
 
-  
-  const key = "627cb640944da56a6800e80a"; // keep it secret
-  
-  async function get_data() {
+let query = "Habe es gekriegt. Was für ein Papier möchtest du? Bis wann bräuchtest du die Ausdrucke? Ok dann bereite ich die Daten einfach vor. Heute Mittag drucke ich das Foto von Oma. Ich besorge auch einen Bilderrahmen ok?";
+
+
+const key = "627cb640944da56a6800e80a"; // keep it secret
+
+async function get_data() {
     const response = await fetch(
-      "https://api.textgain.com/sentiment?q=" +
+        "https://api.textgain.com/sentiment?q=" +
         query +
         "&lang=" +
         language.de +
         "&key=" +
-        key,
-      {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-        },
-        mode: "cors",
-      }
+        key, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+            },
+            mode: "cors",
+        }
     );
-  
+
     const data = await response.json();
     console.log(data);
-  }
-  
-console.log ('hallo')
+}
 
-  get_data();
+console.log('hallo')
+
+get_data();
---
+++ tests/test_typography_experiments_3/assets/script/script.js
@@ -0,0 +1,34 @@
+var textPath = document.querySelector('#text-path')
+
+function updateTextPathOffset(offset) {
+    textPath.setAttribute('startOffset', offset)
+}
+
+function onScroll() {
+    requestAnimationFrame(function() {
+        updateTextPathOffset(window.scrollY * 2)
+    })
+}
+
+window.addEventListener('scroll', onScroll);
+
+
+const titles = document.querySelectorAll('.anim');
+
+observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+       if (entry.isIntersecting == true) {
+entry.target.classList.add('animate');
+       } else{
+        entry.target.classList.remove('animate');
+       }
+    })
+})
+
+titles.forEach(title => {
+    observer.observe(title);
+})
+
+
+
+
