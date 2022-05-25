e5b915e96ee7d8839ebe7f38a6f5d7185e9cc2c5
chalidbaumann <chalid.baumann@stud.hslu.ch>
2022-05-25 16:12:54 +0200

    buttons
    Co-authored-by: sonjaramseier <sonjaramseier@users.noreply.github.com>
    Co-authored-by: Yyyyaaaannnnoooo <Yyyyaaaannnnoooo@users.noreply.github.com>

--- assets/script/loadText.js
+++ assets/script/loadText.js
@@ -7,16 +7,16 @@ async function printJSON() {
 }
 
 
-
+//Text JSON auf SVG-Pfad tun
 function writeTextOnPath() {
     let chalid = document.getElementById("text-path");
     let sonja = document.getElementById("text-path-two");
-    //console.log(el)
     for (let n = 0; n < json.length; n++) {
+        console.log(json[n]['text'], json[n]['application'])
         let div = document.createElementNS("http://www.w3.org/2000/svg", "tspan");
-         div.innerHTML = json[n].text;
-         div.classList.add(json[n].application);
-         if(json[n].author=='Sonja'){
+         div.textContent += json[n]['text'];
+         div.classList.add(json[n]['application']);
+         if(json[n]['author'] =='Sonja'){
             sonja.appendChild(div);
          }
          else {
@@ -28,25 +28,105 @@ function writeTextOnPath() {
 printJSON();
 
 
-function showWhatsapp(){
-    let sonja = document.getElementById("text-path");
-    for (let i = 0; i < sonja.children.length; i++){
-        if (! (sonja.children[i].classList.contains("Whatsapp")))
-        sonja.children[i].style.opacity = 0;
-    }
-}
 
 
+//ALT-Hanna
+// function showWhatsapp(){
+//     let sonja = document.getElementById("text-path");
+//     for (let i = 0; i < sonja.children.length; i++){
+//         if (! (sonja.children[i].classList.contains("Whatsapp")))
+//         sonja.children[i].style.opacity = 0;
+//     }
+// }
+//
 
 
 //Toggle switch button
+//Button für E-Mail und Word
 var input = document.getElementById('toggleswitch');
-var outputtext = document.getElementById('status');
+console.log(input);
+input.addEventListener('change', function () {
+    if (this.checked) {
+        console.log("aktiv")
+        let chalid = document.getElementById("text-path");
+        let sonja = document.getElementById("text-path-two");
+        //Chalid
+        for (let i = 0; i < chalid.children.length; i++) {
+            console.log(chalid.children[i].classList.contains("Whatsapp"));
+            if (chalid.children[i].classList.contains("Whatsapp")) {
+                chalid.children[i].style.display = "none";
+            }
 
-input.addEventListener('change',function(){
-    if(this.checked) {
-        outputtext.innerHTML = "aktiv";
+        }
+        //Sonja
+        for (let i = 0; i < sonja.children.length; i++) {
+            console.log(sonja.children[i].classList.contains("Whatsapp"));
+            if (sonja.children[i].classList.contains("Whatsapp")) {
+                sonja.children[i].style.display = "none";
+            }
+
+        }
+
     } else {
-        outputtext.innerHTML = "inaktiv";
+        console.log("inaktiv")
+        // show all again
+        let chalid = document.getElementById("text-path");
+        let sonja = document.getElementById("text-path-two");
+        //Chalid
+        for (let i = 0; i < chalid.children.length; i++) {
+            if (chalid.children[i].classList.contains("Whatsapp")) {
+                chalid.children[i].style.display = "block";
+            }
+        }
+        //Sonja
+        for (let i = 0; i < sonja.children.length; i++) {
+            if (sonja.children[i].classList.contains("Whatsapp")) {
+                sonja.children[i].style.display = "block";
+            }
+        }
     }
 });
+
+//Button für Whatsapp
+/*var input = document.getElementById('toggleswitch');
+console.log(input);
+input.addEventListener('change', function () {
+    if (this.checked) {
+        console.log("aktiv")
+        let chalid = document.getElementById("text-path");
+        let sonja = document.getElementById("text-path-two");
+        //Chalid
+        for (let i = 0; i < chalid.children.length; i++) {
+            console.log(chalid.children[i].classList.contains("Ewolack"));
+            if (chalid.children[i].classList.contains("Ewolack")) {
+                chalid.children[i].style.display = "none";
+            }
+        }
+        //Sonja
+        for (let i = 0; i < sonja.children.length; i++) {
+            console.log(sonja.children[i].classList.contains("Ewolack"));
+            if (sonja.children[i].classList.contains("Ewolack")) {
+                sonja.children[i].style.display = "none";
+            }
+
+        }
+
+    } else {
+        console.log("inaktiv")
+        // show all again
+        let chalid = document.getElementById("text-path");
+        let sonja = document.getElementById("text-path-two");
+        //Chalid
+        for (let i = 0; i < chalid.children.length; i++) {
+            if (chalid.children[i].classList.contains("Ewolack")) {
+                chalid.children[i].style.display = "block";
+            }
+        }
+        //Sonja
+        for (let i = 0; i < sonja.children.length; i++) {
+            if (sonja.children[i].classList.contains("Ewolack")) {
+                sonja.children[i].style.display = "block";
+            }
+        }
+    }
+});*/
