d661e28b4380bc7d0a683d717211fe2033387357
hzuellig <zuellig@internauta.ch>
2022-05-31 10:39:11 +0200

    before pull

--- tests/test_mehrere_tage/assets/script/loadText.js
+++ tests/test_mehrere_tage/assets/script/loadText.js
@@ -1,4 +1,6 @@
 let json;
+let color1 = "rgb(200,10,0)";
+let color2 = "rgb(10,10,255)";
 async function printJSON() {
     const response = await fetch("assets/script/test.json");
     json = await response.json();
@@ -14,15 +16,38 @@ function writeTextOnPath() {
     for (let n = 19; n < 28; n++) {
         console.log(json[n]['text'], json[n]['application'], json[n]['day'], json[n]['subjectivity'], json[n]['polarity'], json[n]['confidence'])
         let div = document.createElementNS("http://www.w3.org/2000/svg", "tspan");
-         div.textContent += json[n]['text'];
-         div.classList.add(json[n]['application']);
-         if(json[n]['author'] =='Sonja'){
+        div.textContent += json[n]['text'];
+        div.classList.add(json[n]['application']);
+        let c = mixColor(json[n]['subjectivity']);
+        div.setAttribute("subjectivityColor", "rgb(" + c[0] + "," + c[1] + "," + c[2] + ")");
+        if (json[n]['author'] == 'Sonja') {
             sonja.appendChild(div);
-         }
-         else {
-            chalid.appendChild(div); 
-         }
+        } else {
+            chalid.appendChild(div);
+        }
     }
+}
+
+function getRGB(str) {
+    var match = str.match(/rgba?\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3})\)?(?:, ?(\d(?:\.\d?))\))?/);
+    return match ? {
+        red: match[1],
+        green: match[2],
+        blue: match[3]
+    } : {};
+}
+
+function mixColor(percentage) {
+    let c1 = getRGB(color1);
+    let c2 = getRGB(color2);
+
+    let colormix = [
+        Math.round((1 - percentage) * c1.red + percentage * c2.red),
+        Math.round((1 - percentage) * c1.green + percentage * c2.green),
+        Math.round((1 - percentage) * c1.blue + percentage * c2.blue)
+    ];
+
+    return (colormix);
 }
 
 printJSON();
@@ -71,7 +96,7 @@ function draw() {
 //Button für Nur E-Mail und Word
 var input = document.getElementById('toggleswitch1');
 console.log(input);
-input.addEventListener('change', function () {
+input.addEventListener('change', function() {
     if (this.checked) {
         console.log("aktiv")
         let chalid = document.getElementById("text-path");
@@ -87,7 +112,7 @@ input.addEventListener('change', function () {
         //Sonja
         for (let i = 0; i < sonja.children.length; i++) {
             console.log(sonja.children[i].classList.contains("Whatsapp"));
-            if (sonja.children[i].classList.contains("Whatsapp")) { 
+            if (sonja.children[i].classList.contains("Whatsapp")) {
                 sonja.children[i].style.display = "none";
             }
 
@@ -95,7 +120,7 @@ input.addEventListener('change', function () {
 
     } else {
         console.log("inaktiv")
-        // show all again
+            // show all again
         let chalid = document.getElementById("text-path");
         let sonja = document.getElementById("text-path-two");
         //Chalid
@@ -119,7 +144,7 @@ input.addEventListener('change', function () {
 //Button für Nur Whatsapp
 var input = document.getElementById('toggleswitch2');
 console.log(input);
-input.addEventListener('change', function () {
+input.addEventListener('change', function() {
     if (this.checked) {
         console.log("aktiv")
         let chalid = document.getElementById("text-path");
@@ -141,7 +166,7 @@ input.addEventListener('change', function () {
 
     } else {
         console.log("inaktiv")
-        // show all again
+            // show all again
         let chalid = document.getElementById("text-path");
         let sonja = document.getElementById("text-path-two");
         //Chalid
@@ -171,19 +196,18 @@ input.addEventListener('change', function() {
         let sonja = document.getElementById("text-path-two");
         //Chalid
         sonja.style.display = "none";
-        }
-    else {
+    } else {
         console.log("inaktiv")
             // show all again
         let chalid = document.getElementById("text-path");
         let sonja = document.getElementById("text-path-two");
         //Chalid
         sonja.style.display = "block";
-        }
+    }
 });
 
 
-    
+
 //Button für Nur Sonja
 var input = document.getElementById('toggleswitch4');
 console.log(input);
@@ -194,15 +218,14 @@ input.addEventListener('change', function() {
         let sonja = document.getElementById("text-path-two");
         //Sonja
         chalid.style.display = "none";
-        }
-    else {
+    } else {
         console.log("inaktiv")
             // show all again
         let chalid = document.getElementById("text-path");
         let sonja = document.getElementById("text-path-two");
         //Sonja
         chalid.style.display = "block";
-        }
+    }
 });
 
 
@@ -210,31 +233,25 @@ input.addEventListener('change', function() {
 
 
 //Button subjectivity
-/*var input = document.getElementById('toggleswitch5');
+var input = document.getElementById('toggleswitch5');
 console.log(input);
-input.addEventListener('change', function () {
+input.addEventListener('change', function() {
     if (this.checked) {
         console.log("aktiv")
         let chalid = document.getElementById("text-path");
         let sonja = document.getElementById("text-path-two");
         //Chalid
         for (let i = 0; i < chalid.children.length; i++) {
-            console.log(chalid.children[i].classList.contains("Ewolack"));
-            if (chalid.children[i].classList.contains("Ewolack")) {
-                chalid.children[i].style.display = "none";
-            }
+            chalid.children[i].style.fill = chalid.children[i].getAttribute("subjectivityColor");
         }
         //Sonja
         for (let i = 0; i < sonja.children.length; i++) {
-            console.log(sonja.children[i].classList.contains("Ewolack"));
-            if (sonja.children[i].classList.contains("Ewolack")) {
-                sonja.children[i].style.display = "none";
-            }
+            sonja.children[i].style.fill = sonja.children[i].getAttribute("subjectivityColor")
         }
 
     } else {
         console.log("inaktiv")
-        // show all again
+            // show all again
         let chalid = document.getElementById("text-path");
         let sonja = document.getElementById("text-path-two");
         //Chalid
@@ -250,7 +267,4 @@ input.addEventListener('change', function () {
             }
         }
     }
-});*/
-
-
-
+});
